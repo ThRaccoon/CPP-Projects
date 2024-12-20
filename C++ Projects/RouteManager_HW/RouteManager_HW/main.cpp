5fde53c862edb0cb2c6ed4a443c4cfdd0360cf31
Ivan Stoyanov <136110918+ThRaccoon@users.noreply.github.com>
2024-12-20 20:30:38 +0200

    Add files via upload

---
+++ C++ Projects/RouteManager_HW/RouteManager_HW/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+#include "RouteManager.h"
+
+
+// If we want the sorting order to be descending, not ascending
+// we should not use "Min priority queue", but we should use
+// "Max priority queue" or in other words we should change
+// line 52 in the PriorityQueue.h file from >= to <=.
+
+int main()
+{
+    RouteManager rm;
+    
+    return 0;
+}
